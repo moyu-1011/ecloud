59e79ad19c68d3c30dd19549409b29a281fa52c0
moyu-1011 <1011847996@qq.com>
2021-07-01 00:23:06 +0800

    多线程处理上传;删除多余静态资源

--- src/main/java/com/ecloud/app/controller/FileController.java
+++ src/main/java/com/ecloud/app/controller/FileController.java
@@ -7,7 +7,6 @@ import com.ecloud.app.service.ECloudService;
 import com.ecloud.app.service.FaceDetectService;
 import com.ecloud.app.service.ObjectClassicService;
 import com.ecloud.app.service.UniversalDetectService;
-import org.apache.commons.lang.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +17,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class FileController {
@@ -36,81 +37,85 @@ public class FileController {
     // 图片上传
     @PostMapping("/file/upload")
     public String fileUpload(@RequestParam("upload_img") MultipartFile[] files) {
-        String classic = null;
+        ExecutorService exec = Executors.newCachedThreadPool(); // 缓冲线程池
+        long d1 = System.currentTimeMillis();
 
         for (MultipartFile file : files) {
-            String name = file.getOriginalFilename();
+            exec.submit(() -> {
+                String classic = null;  // 图像类别
+                String fileName = file.getOriginalFilename();   // 文件名
+                String savePath = System.getProperty("user.dir") + "/imgs"; // 保存路径
+                String filePath = savePath + "/" + fileName;    // 保存的全路径
+                String newFileName = fileName.substring(0, fileName.lastIndexOf(".")) + "x" + ".jpg";   // 格式转换后名字
+                String newFilePath = savePath + "/" + newFileName;  // 转换后全路径
 
-            if (file.getSize() == 0) {
-                logger.error("ERROR!!!FILE CAN'T BE NULL!");
-                continue;
-            }
-
-            // 保存路径
-            String savePath = System.getProperty("user.dir") + "/imgs";
-            // 文件全路径
-            String filePath = savePath + "/" + name;
-            // 转换格式后的图片: 原图片名x.jpg
-            String newFileName = name.substring(0, name.lastIndexOf(".")) + "x" + ".jpg";
-            // 转换格式后的图片全路径
-            String newFilePath = savePath + "/" + newFileName;
-            FileInputStream in1 = null;
-            FileInputStream in2 = null;
-            FileInputStream in3 = null;
-            FileInputStream in4 = null;
-            try {
-                // 存储至服务器临时资源
-                boolean saveSuccess = FileUtils.saveAsLocal(file);
-                logger.info("'{}' 保存状态: {}'", name, saveSuccess);
-
-                // 图片格式转换
-                JPython.convertImageFile(filePath);
-
-                // 获取转换格式后的输入流
-                // 移动云api入参传入被读取过的输入流时，会报错
-                in1 = new FileInputStream(newFilePath);
-                in2 = new FileInputStream(newFilePath);
-                in3 = new FileInputStream(newFilePath);
-                in4 = new FileInputStream(newFilePath);
-                long size = new File(newFilePath).length();
-
-                // 上传至对象存储资源
-                boolean containsFace = faceDetectService.faceDetect(in1);
-                if (containsFace) {
-                    classic = "human";
-                    cloudService.objectUpload("human", newFilePath, in2, size);
-                    logger.info("对象'{}', 检测到人脸，归类为human", name);
-                } else {
-                    String base64 = Base64Utils.toBase64(in3);
-                    String detectName = universalDetectService.universalDetect(base64);
-                    // 查询该物品所属类别
-                    classic = objectClassicService.findClassicByName(detectName);
-                    // 没有查询到该对象所属何类，归在其他类
-                    classic = classic == null ? "others" : classic;
-                    logger.info(" 对象'{}', 通用识别物品名称: {}, 归类为:  {}", newFilePath, detectName, classic);
-                    cloudService.objectUpload(classic, name, in4, size);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
+                FileInputStream in1 = null;
+                FileInputStream in2 = null;
+                FileInputStream in3 = null;
+                FileInputStream in4 = null;
                 try {
-                    in1.close();
-                    in2.close();
-                    in3.close();
-                    in4.close();
+                    // 存储至服务器临时资源
+                    boolean saveSuccess = FileUtils.saveAsLocal(file);
+                    logger.info("'{}' 保存状态: {}", fileName, saveSuccess);
+
+                    // 图片格式转换
+                    JPython.convertImageFile(filePath);
+
+                    // 获取转换格式后的输入流
+                    // 移动云api入参传入被读取过的输入流时，会报错
+                    in1 = new FileInputStream(newFilePath);
+                    in2 = new FileInputStream(newFilePath);
+                    in3 = new FileInputStream(newFilePath);
+                    in4 = new FileInputStream(newFilePath);
+                    long size = new File(newFilePath).length();
+
+                    // 上传至对象存储资源
+                    boolean containsFace = faceDetectService.faceDetect(in1);
+                    if (containsFace) {
+                        classic = "human";
+                        cloudService.objectUpload("human", newFilePath, in2, size);
+                        logger.info("对象'{}', 检测到人脸，归类为human", fileName);
+                    } else {
+                        String base64 = Base64Utils.toBase64(in3);
+                        String detectName = universalDetectService.universalDetect(base64);
+                        // 查询该物品所属类别
+                        classic = objectClassicService.findClassicByName(detectName);
+                        // 没有查询到该对象所属何类，归在其他类
+                        classic = classic == null ? "others" : classic;
+                        logger.info(" 对象'{}', 通用识别物品名称: {}, 归类为:  {}", newFilePath, detectName, classic);
+                        cloudService.objectUpload(classic, fileName, in4, size);
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    try {
+                        in1.close();
+                        in2.close();
+                        in3.close();
+                        in4.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
 
-            boolean b1 = new File(filePath).delete();
-            logger.info("delete '{}': {}", filePath, b1);
-            if (!classic.equals("human")) {
-                boolean b2 = new File(newFilePath).delete();
-                logger.info("delete '{}': {}", newFilePath, b2);
-            }
+                boolean b1 = new File(filePath).delete();
+                logger.info("delete '{}': {}", filePath, b1);
+                if (!classic.equals("human")) {
+                    boolean b2 = new File(newFilePath).delete();
+                    logger.info("delete '{}': {}", newFilePath, b2);
+                }
+            });
         }
 
-        return "redirect:/pages/widgets/" + classic;
+        try {
+            exec.shutdown();
+            exec.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        logger.info("upload file numbers: {}, cost: {}ms", files.length, System.currentTimeMillis() - d1);
+
+        return "redirect:/pages/widgets";
     }
 }
